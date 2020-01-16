528949e9a5a4a947ee36a0e270000f75df3e8aac
Yohei Matsumoto <yhmtmt@gmail.com>
2020-01-16 16:19:34 +0900

    Updating filter test scheme

--- res/env.sh
+++ res/env.sh
@@ -2,5 +2,5 @@
 
 cur_path=`pwd`
 
-export PATH=$PATH:$cur_path/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cur_path/lib
+export PATH=${PATH}:${cur_path}/../bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${cur_path}/../lib
