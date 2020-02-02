716819ffd0f7f910b60129a7d95de3d2c13f14d5
yhmtmt <yhmtmt@gmail.com>
2020-02-02 10:21:51 +0900

    eliminating second engine related parameters

--- channels/ch_aws1_ctrl.hpp
+++ channels/ch_aws1_ctrl.hpp
@@ -85,10 +85,9 @@ struct s_aws1_ctrl_inst{
   // aws control input (from aws)
   unsigned char rud_aws;
   unsigned char meng_aws;
-  unsigned char seng_aws;
   
   s_aws1_ctrl_inst(): tcur(0), ctrl_src(ACS_UI), 
-		      rud_aws(127), meng_aws(127), seng_aws(127)
+		      rud_aws(127), meng_aws(127)
   {
   }
 };
@@ -100,7 +99,6 @@ struct s_aws1_ctrl_stat{
   // control output
   unsigned char rud;
   unsigned char meng;
-  unsigned char seng;
   
   // control modes
   e_aws1_ctrl_src ctrl_src;
@@ -108,12 +106,10 @@ struct s_aws1_ctrl_stat{
   // aws control input (from aws)
   unsigned char rud_aws;
   unsigned char meng_aws;
-  unsigned char seng_aws;
 
   // aws remote controller's input (from adc)
   unsigned char rud_rmc;
   unsigned char meng_rmc;
-  unsigned char seng_rmc;
 
   // rudder angle 
   unsigned char rud_sta;     // from adc
@@ -125,13 +121,7 @@ struct s_aws1_ctrl_stat{
   unsigned char meng_nut_rmc;
   unsigned char meng_nub_rmc;
   unsigned char meng_min_rmc;
-  
-  unsigned char seng_max_rmc;
-  unsigned char seng_nuf_rmc;
-  unsigned char seng_nut_rmc;
-  unsigned char seng_nub_rmc;
-  unsigned char seng_min_rmc;
-  
+   
   unsigned char rud_max_rmc;
   unsigned char rud_nut_rmc;
   unsigned char rud_min_rmc;
@@ -147,12 +137,6 @@ struct s_aws1_ctrl_stat{
   unsigned char meng_nub;
   unsigned char meng_min;
 
-  unsigned char seng_max;
-  unsigned char seng_nuf;
-  unsigned char seng_nut;
-  unsigned char seng_nub;
-  unsigned char seng_min;
-  
   unsigned char rud_max;
   unsigned char rud_nut;
   unsigned char rud_min;
@@ -165,15 +149,11 @@ struct s_aws1_ctrl_stat{
   ctrl_src(ACS_UI),
   meng_max_rmc(0x81), meng_nuf_rmc(0x80),  meng_nut_rmc(0x7f),  
   meng_nub_rmc(0x7e), meng_min_rmc(0x7d),  
-  seng_max_rmc(0x81),  seng_nuf_rmc(0x80), seng_nut_rmc(0x7f),
-  seng_nub_rmc(0x7e),  seng_min_rmc(0x7d),
   rud_max_rmc(0x80),  rud_nut_rmc(0x7f),  rud_min_rmc(0x7e),
   rud_sta_max(0xff), rud_sta_nut(0x7f), rud_sta_min(0x00),
-  meng(0x7f),  seng(0x7f),  rud(0x7f),  
+  meng(0x7f), rud(0x7f),  
   meng_max(0x81),meng_nuf(0x80),  meng_nut(0x7f),  
   meng_nub(0x7e), meng_min(0x7d),  
-  seng_max(0x81),  seng_nuf(0x80), seng_nut(0x7f),
-  seng_nub(0x7e),  seng_min(0x7d),
   rud_max(0x80),  rud_nut(0x7f),  rud_min(0x7e),
   rud_sta_out_max(0xff), rud_sta_out_nut(0x7f), rud_sta_out_min(0x00)
   {
@@ -228,7 +208,6 @@ public:
     out << "channel " << m_name 
 	<< " rud " <<  (int) inst.rud_aws 
 	<< " meng "<<  (int) inst.meng_aws 
-	<< " seng "<< (int) inst.seng_aws 
 	<< endl;
   }
   
@@ -247,7 +226,6 @@ public:
       fwrite((void*)&inst.ctrl_src, sizeof(e_aws1_ctrl_src), 1, pf);
       fwrite((void*)&inst.rud_aws, sizeof(unsigned char), 1, pf);
       fwrite((void*)&inst.meng_aws, sizeof(unsigned char), 1, pf);
-      fwrite((void*)&inst.seng_aws, sizeof(unsigned char), 1, pf);
       sz = sizeof(long long) + sizeof(e_aws1_ctrl_src), sizeof(unsigned char) * 3;
       m_tfile = inst.tcur;
       unlock();
@@ -278,9 +256,6 @@ public:
       res = fread((void*)&inst.meng_aws, sizeof(unsigned char), 1, pf);
       if(!res)
 	goto eof;
-      res = fread((void*)&inst.seng_aws, sizeof(unsigned char), 1, pf);
-      if(!res)
-	goto eof;
       sz = sizeof(long long) + sizeof(e_aws1_ctrl_src), sizeof(unsigned char) * 3;
       m_tfile = inst.tcur;
       unlock();			
@@ -294,7 +269,7 @@ public:
   virtual bool log2txt(FILE * pbf, FILE * ptf)
   {
     int sz = 0;
-    fprintf(ptf, "t, acs, rud, meng, seng\n");
+    fprintf(ptf, "t, acs, rud, meng\n");
     while(!feof(pbf)){
       size_t res;
       res = fread((void*)&inst.tcur, sizeof(long long), 1, pbf);
@@ -309,14 +284,11 @@ public:
       res = fread((void*)&inst.meng_aws, sizeof(unsigned char), 1, pbf);
       if(!res)
 	break;
-      res = fread((void*)&inst.seng_aws, sizeof(unsigned char), 1, pbf);
-      if(!res)
-	break;
       sz = sizeof(long long) + sizeof(e_aws1_ctrl_src), sizeof(unsigned char) * 3;
       m_tfile = inst.tcur;
       
-      fprintf(ptf, "%lld, %d, %d, %d, %d\n", inst.tcur, 
-	      (int)inst.ctrl_src, (int)inst.rud_aws, (int)inst.meng_aws, (int)inst.seng_aws);
+      fprintf(ptf, "%lld, %d, %d, %d\n", inst.tcur, 
+	      (int)inst.ctrl_src, (int)inst.rud_aws, (int)inst.meng_aws);
     }
     return true;
   }
@@ -370,8 +342,8 @@ public:
   {
     out << "channel " << m_name << " rud " <<  (int) stat.rud  << " " 
 	<<  (int) stat.rud_aws << " meng " <<  (int) stat.meng << " " 
-	<<  (int) stat.meng_aws << " seng " <<  (int) stat.seng << " " << (int) stat.seng_aws << " rud_sta " 
-	<<  (int) stat.rud_sta << " rud_sta_out " <<  (int) stat.rud_sta_out << endl;
+	<< " rud_sta " <<  (int) stat.rud_sta
+	<< " rud_sta_out " <<  (int) stat.rud_sta_out << endl;
   }
   
   // file writer method
@@ -418,7 +390,7 @@ public:
   virtual bool log2txt(FILE * pbf, FILE * ptf)
   {
     int sz = 0;
-    fprintf(ptf, "t, rud, meng, seng, rud_sta\n");
+    fprintf(ptf, "t, rud, meng, rud_sta\n");
     while(!feof(pbf)){
       lock();
       size_t res = fread((void*)&stat, sizeof(stat), 1, pbf);
@@ -427,7 +399,7 @@ public:
       
       sz += (int)res;
       m_tfile = stat.tcur;
-      fprintf(ptf, "%lld, %d, %d, %d, %d\n", stat.tcur, (int) stat.rud, (int) stat.meng, (int) stat.seng, (int) stat.rud_sta_out);
+      fprintf(ptf, "%lld, %d, %d, %d\n", stat.tcur, (int) stat.rud, (int) stat.meng, (int) stat.rud_sta_out);
       unlock();
     }
     return true;
@@ -448,7 +420,6 @@ protected:
   e_ap_mode mode;
   float smax; // maximum speed
   float meng_max, meng_min; // main engine max/min value
-  float seng_max, seng_min; // sub engine's max/min value
   
   // Stay position (used only in EAP_STAY)
   double lat_stay, lon_stay;
